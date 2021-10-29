865b9da0cff39fd47603b12e9ad7d6ec7d097309
joirneto <joirneto@gmail.com>
2021-10-29 10:29:03 -0300

    add HowToAdded

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PageTitle from "../components/PageTitle"
 import Header from "../components/Header";
+import HowToAdded from "../components/HowToAdded";
 import HowWorks from "../components/HowWorks";
 
 const Index = () =>{
@@ -9,6 +10,9 @@ const Index = () =>{
     
     <PageTitle title = "Seja bem Vindo!"/>
     <Header />
+    <aside className= "w-full md:w-1/3 flex flex-col items-center px-3" >
+      <HowToAdded />
+    </aside>
     <HowWorks />
     </>
   )
