28f23467f05a4403d2d04cdb1ddb78bd27c2a913
joirneto <joirneto@gmail.com>
2021-10-29 21:09:56 -0300

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () =>(
       </div>
       <div><img  src='/logo_ibf.png' height="150" width="150" /> </div>
       <div className="uppercase pb-6"> &copy; precisandodefalecom.vercel.app </div>
-      <div>
+      <div className="items-center justify-between">
       Projeto desenvolvido por:
         Joir Neto / {' '}
         <a className='hover:underline' href="https://linkedin.com/in/joir-neto">Linkedin</a> / {' '}
