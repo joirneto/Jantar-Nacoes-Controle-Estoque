f7f434d8a9bb89d2059df53de32f5e29eaec7847
Joir Neto <joirneto@febracis.com.br>
2023-08-19 23:20:32 -0300

    mais ajustes

--- pages/api/doar.js
+++ pages/api/doar.js
@@ -6,7 +6,7 @@ const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 export default async (req, res) => {
   await doc.useServiceAccountAuth({
     client_email: process.env.SHEET_CLIENT_EMAIL,
-    private_key: process.env.SHEET_PRIVATE_KEY,
+    private_key: fromBase64(process.env.SHEET_PRIVATE_KEY),
   })
   await doc.loadInfo()
   const sheet = doc.sheetsByIndex[1]
